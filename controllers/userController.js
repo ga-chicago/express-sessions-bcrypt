071667caf9b039e78a8a3a88e43d3db31bc82855
Reuben Ayres <ra@sent.com>
2018-04-17 16:18:28 -0500

    added bcrypt for encrypting passwords

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js')
+const bcrypt = require('bcrypt')
 
 router.get('/', (req, res) => {
 	res.send('User controller')	
@@ -23,15 +24,32 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
 
-	// you can add whatever data you want to the session
-	req.session.username = req.body.username;
-	req.session.loggedIn = true;
-	req.session.message = "Thanks for signing up, " + req.body.username;
+	// capture password 
+	const password = req.body.password;
 
-	console.log(req.session, "hey this was logged from within /users/register post route");
+	// 1st param = the pwd you're encrypting
+	// 2nd param is the algorithm we encrypt with (& salt)
+	const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-	res.redirect('/home');
+	// this is the obj we will store in our db
+	const userDbEntry = {
+		username: req.body.username,
+		password: passwordHash
+	}
 
+	User.create(userDbEntry, (err, createdUser) => {
+		console.log(createdUser, " ^^^ this is the user that got created--------------------------------------")
+
+		// you can add whatever data you want to the session
+		req.session.username = createdUser.username
+		req.session.loggedIn = true;
+		req.session.message = "Thanks for signing up, " + req.body.username;
+
+		res.redirect('/home');
+
+	})
+
+ 
 })
 
 module.exports = router;
